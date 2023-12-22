9578e585c3c0091c06239186a5182da54334bfe3
Mkznd <38818285+Mkznd@users.noreply.github.com>
2023-12-22 15:29:32 +0200

    Time: 40 ms (46.08%), Space: 17.4 MB (16.98%) - LeetHub

---
+++ 1221-split-a-string-in-balanced-strings/1221-split-a-string-in-balanced-strings.py
@@ -0,0 +1,14 @@
+class Solution:
+    def balancedStringSplit(self, s: str) -> int:
+        L = 0
+        R = 0
+        res = 0
+        
+        for i in s:
+            if i =="L":
+                L+=1
+            else:
+                R+=1
+            if L == R:
+                res+=1
+        return res
