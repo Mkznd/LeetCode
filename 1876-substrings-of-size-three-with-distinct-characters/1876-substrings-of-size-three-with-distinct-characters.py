d35b2ea08ce1065b0ad9122f510a9c6dc528bbce
Mkznd <38818285+Mkznd@users.noreply.github.com>
2023-12-22 16:34:19 +0200

    Time: 31 ms (95.81%), Space: 17.3 MB (22.49%) - LeetHub

--- 1876-substrings-of-size-three-with-distinct-characters/1876-substrings-of-size-three-with-distinct-characters.py
+++ 1876-substrings-of-size-three-with-distinct-characters/1876-substrings-of-size-three-with-distinct-characters.py
@@ -3,6 +3,5 @@ class Solution:
         res = 0
         for i in range(len(s)-2):
             t = s[i:i+3]
-            if len(t) == len(set(t)):
-                res+=1
+            res+=len(t) == len(set(t))
         return res
