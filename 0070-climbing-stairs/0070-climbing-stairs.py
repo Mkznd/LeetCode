f0ccfa721763eaa95f11267055b5f5a627561d0b
Mkznd <38818285+Mkznd@users.noreply.github.com>
2023-12-29 16:48:08 +0200

    Time: 37 ms (59.91%), Space: 17.3 MB (7.12%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -0,0 +1,13 @@
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        
+        @functools.cache
+        def dfs(n):
+            if n == 0:
+                return 1
+            if n < 0:
+                return 0
+            
+            return dfs(n-1) + dfs(n-2)
+        
+        return dfs(n)
