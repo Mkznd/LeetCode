9ef41bf1a3432ec4f113858d55edc765b1bf6e1a
Mkznd <38818285+Mkznd@users.noreply.github.com>
2023-12-29 16:59:32 +0200

    Time: 84 ms (6.10%), Space: 17.3 MB (7.12%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.py
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -1,17 +1,12 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
-        pre,cur = 0,1
-        for i in range(n):
-            pre,cur = cur, pre+cur
-        return cur
-        
-#         @functools.cache
-#         def dfs(n):
-#             if n == 0:
-#                 return 1
-#             if n < 0:
-#                 return 0
+        @functools.cache
+        def dfs(n):
+            if n == 0:
+                return 1
+            if n < 0:
+                return 0
             
-#             return dfs(n-1) + dfs(n-2)
+            return dfs(n-1) + dfs(n-2)
         
-#         return dfs(n)
+        return dfs(n)
