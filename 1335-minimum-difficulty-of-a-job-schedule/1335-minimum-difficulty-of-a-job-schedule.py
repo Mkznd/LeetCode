e32b16e4e154a0f5163426efc928a3e20bce3073
Mkznd <38818285+Mkznd@users.noreply.github.com>
2023-12-29 16:22:06 +0200

    Time: 95 ms (97.39%), Space: 24.2 MB (5.09%) - LeetHub

--- 1335-minimum-difficulty-of-a-job-schedule/1335-minimum-difficulty-of-a-job-schedule.py
+++ 1335-minimum-difficulty-of-a-job-schedule/1335-minimum-difficulty-of-a-job-schedule.py
@@ -5,15 +5,13 @@ class Solution:
         if sum(jobDifficulty) == 0:
             return 0
         
-        cache = {}
-        
+        @functools.cache
         def dfs(i, d, cur_max):
             if i == len(jobDifficulty):
                 return 0 if d == 0 else inf
             if d == 0:
                 return inf
-            if (i,d,cur_max) in cache.keys():
-                return cache[(i,d,cur_max)]
+
             
             cur_max = max(cur_max, jobDifficulty[i])
             res = min(
@@ -21,7 +19,6 @@ class Solution:
                 cur_max + dfs(i+1, d-1, -1)
             )
             
-            cache[(i,d,cur_max)] = res
             return res
             
         
