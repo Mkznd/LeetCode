1762bdbf3dd6c70d28aca1b87a9afbaaebb0891a
Mkznd <38818285+Mkznd@users.noreply.github.com>
2024-02-07 15:14:29 +0100

    Time: 88 ms (71.67%), Space: 20.6 MB (55.91%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -1,13 +1,8 @@
+from collections import defaultdict
+
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        a = {} # sorted() : index in res
-        res = []
-        ind = 0
-        for i in strs:
-            temp = str(sorted(i))
-            if temp in a.keys():
-                res[a[temp]].append(i)
-            else:
-                a[temp] = len(res)
-                res.append([i])
-        return res
+        letters_to_words = defaultdict(list)
+        for word in strs:
+            letters_to_words[tuple(sorted(word))].append(word)
+        return list(letters_to_words.values())
