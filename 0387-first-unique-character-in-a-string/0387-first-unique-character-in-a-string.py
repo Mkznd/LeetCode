30c6ec1a629ddfa158a6845827cba08e1bfa4143
Mkznd <38818285+Mkznd@users.noreply.github.com>
2024-02-07 15:36:14 +0100

    Time: 95 ms (47.07%), Space: 16.6 MB (95.57%) - LeetHub

--- 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.py
+++ 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.py
@@ -6,6 +6,4 @@ class Solution:
                 a[i] = index
             else:
                 a[i] = -1
-        if all(i == -1 for i in a.values()):
-            return -1
-        return sorted(i for i in a.values() if i != -1)[0]
+        return min([i for i in a.values() if i!=-1], default=-1)
